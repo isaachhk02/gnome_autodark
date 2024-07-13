9ad74387af0e72912b637b9f923d89bd81a09174
Isaac Ordoñez Fornós <isaac.fornos02@gmail.com>
2024-07-13 19:11:20 +0200

    Add files via upload

--- src/daemon.rs
+++ src/daemon.rs
@@ -2,12 +2,12 @@ use std::process::Command;
 use chrono::prelude::*;
 
 pub fn start_daemon() {
-    let time_n: chrono::DateTime<Utc> = Utc::now();
-    if time_n.hour() >= 18 {
+    let time_n: chrono::DateTime<Local> = Local::now();
+    if time_n.hour12() >= (true,07) {
         println!("Changing to Dark mode!");
         let _dark_mode = Command::new("bash").args(["-c","gsettings set org.gnome.desktop.interface color-scheme prefer-dark"]).output().expect("Error to change to dark mode");
     }
-    if time_n.hour() == 07 {
+    if time_n.hour12() <= (false,07) {
         println!("Changing to Light mode!");
         let _light_mode = Command::new("bash").args(["-c","gsettings set org.gnome.desktop.interface color-scheme prefer-light"]).output().expect("Error to change to light mode");
     }
